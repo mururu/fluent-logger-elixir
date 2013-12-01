69bcfda01f5ef51d77e3428dcdc293f01c355138
Yuki Ito <yuki@gnnk.net>
2013-12-01 17:48:48 +0900

    Implement retry

--- lib/fluent/event.ex
+++ lib/fluent/event.ex
@@ -1,6 +1,6 @@
-defexception Fluent.ConnectionError, host: nil, port: nil do
+defexception Fluent.ConnectionError, host: nil, port: nil, reason: "" do
   def message(exception) do
-    "cannot connect to #{exception.host}:#{exception.port}"
+    "cannot connect to #{exception.host}:#{exception.port} by #{exception.reason}"
   end
 end
 
@@ -10,13 +10,14 @@ defmodule Fluent.Event do
   defrecordp :state, [:tag, :host, :port, :socket]
 
   def init({tag, host, port}) do
-    { :ok, socket } = :gen_tcp.connect(String.to_char_list!(host), port, [:binary, { :packet, 0 }])
+    { :ok, host } = String.to_char_list(host)
+    { :ok, socket } = :gen_tcp.connect(host, port, [:binary, { :packet, 0 }])
     { :ok, state(tag: tag, host: host, port: port, socket: socket) }
   end
 
   def handle_event({ tag, data }, state() = s) when is_list(data) do
     content = make_content(tag, {data}, s)
-    send(content, s)
+    send(content, s, 3)
   end
 
   defp make_content(tag, data, state(tag: top_tag)) do
@@ -31,10 +32,19 @@ defmodule Fluent.Event do
     if top_tag, do: "#{top_tag}.#{tag}", else: tag
   end
 
-  defp send(content, state(socket: socket, host: host, port: port) = s) do
+  defp send(_content, state(host: host, port: port), 0) do
+    raise Fluent.ConnectionError, host: host, port: port, reason: "retry limit"
+  end
+
+  defp send(content, state(socket: socket, host: host, port: port) = s, count) do
     case :gen_tcp.send(socket, content) do
-      :ok -> { :ok, s }
-      _ -> raise FluentLogger.ConnectionError, host: host, port: port
+      :ok ->
+        { :ok, s }
+      { :error, :closed } ->
+        { :ok, socket } =:gen_tcp.connect(host, port, [:binary, { :packet, 0 }])
+        send(content, state(s, socket: socket), count - 1)
+      { :error, reason } ->
+        raise Fluent.ConnectionError, host: host, port: port, reason: atom_to_binary(reason)
     end
   end
 
